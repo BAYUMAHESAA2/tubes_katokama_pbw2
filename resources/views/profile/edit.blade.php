233e0f89733fa8ea125afeb1bd95a1f9e7ed177b
Bagas284 <bagasaldianata20@gmail.com>
2024-12-06 16:49:46 +0700

    edit tampilan

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,33 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
+    <div class="row border border-secondary-subtle rounded-4 mx-5 my-4 shadow-sm">
+        <div class="col-12 col-md-3 d-flex flex-column justify-content-center align-items-center py-4">
+            <!-- Profile Image -->
+                <div class="mb-4">
+                    <img src="{{ asset('img/foto_profil.png') }}" alt="Profile" class="rounded-circle" style="width: 140px; height: 140px; object-fit: cover;">
                 </div>
-            </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
+                <!-- Button -->
+                <div>
+                    <button type="button" class="btn btn-warning">Unggah foto</button>
                 </div>
-            </div>
+        </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
+
+        <div class="col">
+            @include('profile.partials.update-profile-information-form')
         </div>
     </div>
+
+    <div class="row border border-secondary-subtle rounded-4 mx-5 my-4 shadow-sm">
+        @include('profile.partials.update-password-form')
+    </div>
+
+    <div class="row border border-secondary-subtle rounded-4 mx-5 my-4 shadow-sm">
+        @include('profile.partials.delete-user-form')
+    </div>
+
+    <form method="POST" action="{{ route('logout') }}" class="mx-5 mb-4">
+        @csrf
+        <button type="submit" class="btn btn-outline-warning w-100">Keluar</button>
+    </form>
 </x-app-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,16 +6,33 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name') }}</title>
+        <link rel="shortcut icon" type="image/png/jpg" href="img/logo_FoodExplore.png">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="shortcut icon" type="image/png/jpg" href="img/logo_FoodExplore.png">
+
+        <link href="https://fonts.googleapis.com/css2?family=Lobster+Two:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Lobster+Two&family=Squada+One&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
         <style>
+            .f-selamatDatang {
+                font-family: "Lobster Two", sans-serif;
+                font-weight: 400;
+                font-style: normal;
+                margin-bottom: -13px;
+            }
+
+            .f-temukan {
+                font-family: "Squada One", sans-serif;
+                font-weight: 400;
+                font-style: normal;
+                margin-bottom: -3px; 
+            }
+
             .bg-shape {
                 clip-path: polygon(0 0, 80% 0, 60% 100%, 0% 100%);
                 height: 100vh;
@@ -24,13 +41,14 @@
             @media (max-width: 795px) {
                 .bg-shape {
                     clip-path: none;
-                    height: 55vh;
+                    height: 65vh;
                     width: 100%;
                     align-items : center;
                 }
             }
         </style>
 
+        <!-- Animasi -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
     </head>
 
@@ -42,30 +60,35 @@
         
             <div class="row position-relative z-1">
                 <div class="col-10 bg-merahtua bg-opacity-75 bg-shape d-flex flex-column justify-content-center">
-                    @guest
-                    <div class="col-7 d-flex flex-column align-items-center justify-content-center">
-                        <div class="mb-4 text-center">
-                            <p class="display-2 fw-bold mb-1 text-warning animate__animated animate__fadeIn">
+                    <div class="col-8 d-flex flex-column align-items-center justify-content-center">
+                        <div class="text-center">
+                            <p class="f-selamatDatang fs-2 text-warning animate__animated animate__zoomIn">
                                 Selamat Datang
                             </p>
 
-                            <p class="fs-4 fw-medium text-white mb-0 animate__animated animate__fadeIn animate__delay-1s">
-                                Temukan makanan yang anda inginkan !!!
+                            <p class="f-temukan text-white display-5 animate__animated animate__fadeIn animate__delay-1s">
+                                Temukan makanan yang anda inginkan
                             </p>
 
-                            <p class="text-light mb-0 animate__animated animate__fadeIn animate__delay-2s">
+                            <p class="text-white-50 animate__animated animate__fadeIn animate__delay-2s">
                                 Terdapat berbagai macam informasi yang tersedia di dalamnya
                             </p>
                         </div>
 
+                    @guest
                         <div class="animate__animated animate__pulse animate__slow animate__infinite">
-                            <a href="{{ route('login') }}" class="btn btn-outline-warning btn-lg me-3" aria-label="Login">Login</a>
+                            <a href="{{ route('login') }}" class="btn btn-outline-warning btn-md me-3" aria-label="Login">Login</a>
         
-                            <a href="{{ route('register') }}" class="btn btn-warning btn-lg" aria-label="Register">Register</a>
+                            <a href="{{ route('register') }}" class="btn btn-warning btn-md" aria-label="Register">Register</a>
                         </div>
                     </div>
+
+                    @else
+                    <div class="animate__animated animate__pulse animate__slow animate__infinite">
+                            <a href="{{ route('dashboard') }}" class="btn btn-outline-warning btn-md me-3 w-100" aria-label="Login">Masuk</a>
+                        </div>
                     @endguest
-                    
+
                 </div>
             </div>
         </div>
